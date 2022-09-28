c655ea8dae77df1746dc9bf1ba930f042fe8c271
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-28 19:50:51 -0400

    implementação contatoModel

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -1,9 +1,18 @@
-﻿namespace ControleDeContatos.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models {
     public class ContatoModel 
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do contato")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o email do contato")]
+        [EmailAddress(ErrorMessage = "O email informado não é valido")]
         public String Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o celular do contato")]
         public String Celular { get; set; }
     }
 }
