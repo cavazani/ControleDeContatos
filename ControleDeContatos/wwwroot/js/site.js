339089b95edae5c4464f6bf6365d9045ff8db8a6
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-29 21:18:09 -0400

    implementando paginação

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -3,6 +3,11 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    $('#table-contatos').DataTable();
+});
+
+
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
